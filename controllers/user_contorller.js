42384acd6bf1c78974de7a5a84d3a0469a76ac3a
Md Azaz <mdazaz9026@gmail.com>
2024-10-02 18:34:40 +0530

    deleteuser

--- controllers/user_contorller.js
+++ controllers/user_contorller.js
@@ -34,3 +34,82 @@ exports.createUser = async (req, res) => {
         });
     }
 }
+
+
+
+exports.getUsers = async (req , res) => {
+    try{
+        const users = await User.find({});
+
+        if(!users){
+            res.status(500).json({
+                success: false,
+                message: "!NO User Found....",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Users fetched successfully',
+            users,
+        });
+
+    }
+    catch(error){
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+exports.deleteUser = async(req , res) => {
+    try{
+        const userId = req.params.id;
+
+        // Check if the user exists
+        if (!userId){
+            res.status(500).json({
+                success: false,
+                message: error.message,
+            });    
+        }
+
+        // Proceed to delete the user
+        await User.findByIdAndDelete(userId)
+        res.status(200).json({
+        success: true,
+        message: 'User deleted successfully',
+    });
+    }
+    catch(error){
+        // Handle errors appropriately
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+
+exports.editUser = async(req, res) => {
+    try{
+
+        // Proceed to delete the user
+        const user = await User.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+        success: true,
+        message: 'User Updated successfully',
+
+    });
+    }
+    catch(error){
+        // Handle errors appropriately
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { home, createUser } = require("../controllers/user_contorller.js"); // Fixed typo
+const { home, createUser, getUsers, editUser, deleteUser } = require("../controllers/user_contorller.js"); // Fixed typo
 
 const router = express.Router();
 
 // Define routes
 router.get("/", home);
 router.post("/users", createUser); // Changed to /users for consistency
+router.get("/getusers", getUsers)
+router.put("/edituser/:id", editUser)
+router.delete("/deleteuser/:id", deleteUser)
+
+
 
 module.exports = router;
